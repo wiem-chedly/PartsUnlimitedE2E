14eeda49446d300fa3690670834ca5b6ffb2c09d
Wiem <wiemchedlyti21@gmail.com>
2025-11-16 17:17:18 +0100

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // My first chage.
+        // My second change.
         [Key]
         public int CartItemId { get; set; }
 
